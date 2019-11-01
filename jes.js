211368968e76722251ec538485e572c83729eb4e
mcbarnhart <54322943+mcbarnhart@users.noreply.github.com>
2019-11-01 11:15:18 -0500

    added atmosphere

--- jes.js
+++ jes.js
@@ -1,11 +1,11 @@
-var xMax = 600;
+var xMax = 1550;
 var yMax = 600;
 var wMax = 150;
 var hMax = 120;
 var cMax = 256;
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(1550, 600);
     noStroke();
     noLoop();
 }
@@ -30,13 +30,14 @@ function randomC(cMax) {
     return Math.floor(Math.random() * Math.floor(cMax));
 }
 
-function draw() {
+function drawFish() {
     var xFish = randomX(xMax);
     var yFish = randomY(yMax);
     var wFish = randomW(wMax);
     var hFish = randomH(hMax);
     var wTail = wFish / 4;
     var hTail = hFish / 2;
+    noStroke();
     fill(randomC(cMax), randomC(cMax), randomC(cMax));
     ellipse(xFish, yFish, wFish, hFish);
     triangle(xFish - wFish / 2, yFish, xFish - wFish / 2 - wTail, yFish - hTail, xFish - wFish / 2 - wTail, yFish + hTail);
@@ -45,5 +46,41 @@ function draw() {
 } 
 
 function keyPressed() {
-    redraw();
+    drawFish();
+}
+
+function mousePressed() {
+    drawBubble();
+    drawWeeds();
+}
+
+function mouseReleased() {
+    drawWeeds();
+}
+
+function draw() {
+    stroke(255, 190, 0);
+    strokeWeight(20);
+    line(0, 600, 1550, 600);
+}
+
+function drawWeeds() {
+    noStroke();
+    fill(50, randomC(cMax), 0);
+    arc(randomX(xMax), 597, 10, 100, 525, false);
+    fill(0, randomC(cMax), 60);
+    arc(randomX(xMax), 595, 20, 200, 550, false);
+    fill(0, randomC(cMax), 80);
+    arc(randomX(xMax), 595, 15, 300, 550, false);
+}
+
+function drawBubble() {
+    noStroke();
+    fill (150, 150, 150);
+    arc(randomX(xMax), 595, 40, 45, 550, false);
+    stroke(255, 255, 255);
+    strokeWeight(2);
+    noFill();
+    // fill(160, 220, 250);
+    ellipse(randomX(xMax), randomY(yMax), 30, 30);
 }
