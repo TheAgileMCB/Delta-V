2ac52ac772504f9fd50b05c4f2b4b85f6e0c93c7
mcbarnhart <54322943+mcbarnhart@users.noreply.github.com>
2019-11-17 18:20:09 -0600

    replaced bubbles function with fish!

--- bubbles.js
+++ bubbles.js
@@ -1,54 +1,62 @@
-var bubbles = [];
+var fishes = [];
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1000, 600);
     for (var i = 0; i < 30; i++) {
         let x = random(width);
         let y = random(height);
-        let r = random(40, 70);
-        let b = new Bubble(x, y, r);
-        bubbles.push(b);
+        let w = random(40, 150);
+        let h = random(30, 120);
+        let wTail = w/4;
+        let hTail = h/2;
+        let b = new Fish(x, y, w, h, wTail, hTail);
+        fishes.push(b);
     }
-
-
 }
 
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+//  }
+
 function mousePressed() {
-    for (let i = bubbles.length-1; i >= 0; i--) {
-        if (bubbles[i].contains(mouseX, mouseY)) {
-            bubbles.splice(i, 1);
+    for (let i = fishes.length-1; i >= 0; i--) {
+        if (fishes[i].contains(mouseX, mouseY)) {
+            fishes.splice(i, 1);
         }
     }
 }
 
 function draw() {
-    background(0);
-    for (let i = 0; i < bubbles.length; i++) {
-        if (bubbles[i].contains(mouseX, mouseY)) {
-            bubbles[i].changeColor(255);
-        } else {
-            bubbles[i].changeColor(0);
-        }
-        bubbles[i].move();
-        bubbles[i].show();
-    }
+    // "document.querySelector('style.canvas.background
+//     noStroke();
+// colorMode(RGB, 150);
+// for (let k = 0; k < 255; k++) {
+//   for (let j = 0; j < 255; j++) {
+//         stroke(0, j, k);
+//         point(k, j);
+//         }
+//     };
+background(20,150,255);
+    for (let i = 0; i < fishes.length; i++) {
+        fishes[i].move();
+        fishes[i].show();
+    };
 }
 
-class Bubble {
-    constructor(x, y, r) {
+class Fish {
+    constructor(x, y, w, h, wTail, hTail) {
         this.x = x;
         this.y = y;
-        this.r = r;
-        this.brightness = 0;
+        this.w = w;
+        this.h = h;
+        this.wTail = wTail;
+        this.hTail = hTail;
     }
 
-    changeColor(bright) {
-        this.brightness = bright;
-    }
 
     contains(px, py) {
         let d = dist(px, py, this.x, this.y);
-        if(d < this.r) {
+        if(d < this.w) {
             return true;
         } else {
             return false;
@@ -56,14 +64,16 @@ class Bubble {
     }
 
     move() {
-        this.x = this.x + random(-2, 2);
-        this.y = this.y + random(-2, 2);
+        this.x = this.x + random(-2, 4);
+        this.y = this.y + random(-2, 4);
     }
 
     show() {
-        stroke(255);
-        fill(this.brightness);
-        // tint(255, 255);
-        ellipse(this.x, this.y, this.r);
+        noStroke();
+        fill(200, 100, 20);
+        ellipse(this.x, this.y, this.w, this.h);
+        triangle(this.x - this.w / 2, this.y, this.x - this.w / 2 - this.wTail, this.y - this.hTail, this.x - this.w / 2 - this.wTail, this.y + this.hTail);
+        fill(255);
+        ellipse(this.x + this.w / 4, this.y, this.h / 5, this.h / 5);
     }
 }
