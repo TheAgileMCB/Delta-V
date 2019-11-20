888ed494fd54660d6cd74b3142f35b6348e8c2b3
mcbarnhart <54322943+mcbarnhart@users.noreply.github.com>
2019-11-20 13:30:47 -0600

    added color variable to Fish function

--- bubbles.js
+++ bubbles.js
@@ -1,4 +1,6 @@
 var fishes = [];
+var colors = ["red", "blue", "pink", "yellow", "green", "skyblue", "white", "purple", "orange", "grey"];
+
 
 function setup() {
     createCanvas(1000, 600);
@@ -9,7 +11,8 @@ function setup() {
         let h = random(30, 120);
         let wTail = w/4;
         let hTail = h/2;
-        let b = new Fish(x, y, w, h, wTail, hTail);
+        let color = random(colors);
+        let b = new Fish(x, y, w, h, wTail, hTail, color);
         fishes.push(b);
     }
 }
@@ -28,15 +31,21 @@ function mousePressed() {
 
 function draw() {
     // "document.querySelector('style.canvas.background
-//     noStroke();
-// colorMode(RGB, 150);
-// for (let k = 0; k < 255; k++) {
-//   for (let j = 0; j < 255; j++) {
-//         stroke(0, j, k);
-//         point(k, j);
+   
+// for(var pixelW = 0; pixelW <= 1000; pixelW++) {
+//     for(var pixelH = 0; pixelH <= 600; pixelH++) {
+//         noStroke();
+//         colorMode(RGB, 150);
+//         for (let k = 0; k < 255; k++) {
+//           for (let j = 0; j < 255; j++) {
+//                 stroke(0, j, k);
+//                 point(pixelW, pixelH);
+//                 }
+//             };
 //         }
-//     };
-background(20,150,255);
+//     }
+
+        background(20,150,255);
     for (let i = 0; i < fishes.length; i++) {
         fishes[i].move();
         fishes[i].show();
@@ -44,13 +53,14 @@ background(20,150,255);
 }
 
 class Fish {
-    constructor(x, y, w, h, wTail, hTail) {
+    constructor(x, y, w, h, wTail, hTail, color) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.wTail = wTail;
         this.hTail = hTail;
+        this.color = color;
     }
 
 
@@ -64,16 +74,16 @@ class Fish {
     }
 
     move() {
-        this.x = this.x + random(-2, 4);
-        this.y = this.y + random(-2, 4);
+        this.x = this.x + random(-2, 6);
+        this.y = this.y + random(-1, 1);
     }
 
     show() {
         noStroke();
-        fill(200, 100, 20);
+        fill(this.color);
         ellipse(this.x, this.y, this.w, this.h);
         triangle(this.x - this.w / 2, this.y, this.x - this.w / 2 - this.wTail, this.y - this.hTail, this.x - this.w / 2 - this.wTail, this.y + this.hTail);
-        fill(255);
+        fill(0);
         ellipse(this.x + this.w / 4, this.y, this.h / 5, this.h / 5);
     }
 }
